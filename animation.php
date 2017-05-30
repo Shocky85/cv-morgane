a57995a129d3359687e945a52bc791c8d88c2c98
Aurélien FOUQUET <Aurlien FOUQUET>
2017-05-30 15:39:34 +0200

    modif name

--- animation.php
+++ animation.php
@@ -32,7 +32,7 @@
 	.dollar:before {
 		content: '$';
 		color: #52a563;
-		
+
 	}
 	.var{
 		color: #52a563;
@@ -66,7 +66,7 @@
 				<span class="dollar"></span>
 				<span class="var">name</span>
 				<span class="sign">=</span>
-				<span class="value">"Morgane PARENT"</span>
+				<span class="value">"Aurélien FOUQUET"</span>
 				;
 				<br/>
 
@@ -103,13 +103,13 @@
 				<span class="value">"MySQL"</span>
 				);
 				<br/>
-			
+
 			</div>
 
 			<div class="center">
 				<svg id="hands-svg" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="196px" height="196px" viewBox="0 0 196 196" enable-background="new 0 0 196 196" xml:space="preserve">
 					<g id="keyboard_full">
-						<g id="keyboard"> 
+						<g id="keyboard">
 
 							<rect id="body" x="21" y="77" fill="#FFFFFF" stroke="#010202" stroke-width="3" stroke-linecap="square" stroke-miterlimit="10" width="153" height="57"/>
 							<g id="keys">
@@ -164,11 +164,11 @@
 							<path fill="#FFFFFF" stroke="#3D3D3D" stroke-width="3" stroke-miterlimit="1" d="M66.64,168.001v-4l12.364-28.163
 							c0.995-2.087,1.27-3.829-0.319-5.413s-4.185-2.049-5.867,0.729l-5.317,8.847v-4H43.138V123.69c0-2.076-1.821-3.759-4.067-3.759
 							c-2.247,0-4.067,1.683-4.067,3.759v20.341l0.001,0.001V152l6.327,16"/>
-							<path fill="#FFFFFF" stroke="#3D3D3D" stroke-width="3" stroke-miterlimit="1" d="M59.408,119.206v18.833"/> 
+							<path fill="#FFFFFF" stroke="#3D3D3D" stroke-width="3" stroke-miterlimit="1" d="M59.408,119.206v18.833"/>
 							<path id="left_index_finger" fill="#FFFFFF" stroke="#3D3D3D" stroke-width="3" stroke-miterlimit="1" d="M67.5,144.031
 							l0.043-24.795c0-2.074-1.82-3.602-4.066-3.602c-2.247,0-4.068,1.529-4.068,3.602v18.834"/>
 							<path id="left_middle_finger" fill="#FFFFFF" stroke="#3D3D3D" stroke-width="3" stroke-miterlimit="1" d="M59.374,138.074
-							v-22.436c0-2.074-1.82-3.604-4.066-3.604l0,0c-2.247,0-4.068,1.529-4.068,3.604v22.435"/>  
+							v-22.436c0-2.074-1.82-3.604-4.066-3.604l0,0c-2.247,0-4.068,1.529-4.068,3.604v22.435"/>
 							<path id="left_ring_finger" fill="#FFFFFF" stroke="#3D3D3D" stroke-width="3" stroke-miterlimit="1" d="M51.239,138.071v-18.833
 							c0-2.074-1.821-3.604-4.067-3.604l0,0c-2.247,0-4.067,1.53-4.067,3.604v18.833"/>
 						</g>
@@ -176,13 +176,13 @@
 							<path fill="#FFFFFF" stroke="#3D3D3D" stroke-width="3" stroke-miterlimit="1" d="M128.177,168.001v-4l-12.364-28.163
 							c-0.995-2.087-1.27-3.829,0.319-5.413s4.185-2.049,5.867,0.729l5.317,8.847v-4h24.362V123.69c0-2.076,1.821-3.759,4.067-3.759
 							c2.247,0,4.067,1.683,4.067,3.759v20.341l-0.001,0.001V152l-6.327,16"/>
-							<path fill="#FFFFFF" stroke="#3D3D3D" stroke-width="3" stroke-miterlimit="1" d="M135.408,138.039v-18.833"/> 
+							<path fill="#FFFFFF" stroke="#3D3D3D" stroke-width="3" stroke-miterlimit="1" d="M135.408,138.039v-18.833"/>
 							<path id="right_index_finger" fill="#FFFFFF" stroke="#3D3D3D" stroke-width="3" stroke-miterlimit="1" d="M135.408,138.07
 							v-18.834c0-2.072-1.821-3.602-4.068-3.602c-2.246,0-4.066,1.527-4.066,3.602l0.043,24.795"/>
 							<path id="right_middle_finger" fill="#FFFFFF" stroke="#3D3D3D" stroke-width="3" stroke-miterlimit="1" d="M143.577,138.073
-							v-22.435c0-2.074-1.821-3.604-4.068-3.604l0,0c-2.246,0-4.066,1.529-4.066,3.604v22.436"/> 
+							v-22.435c0-2.074-1.821-3.604-4.068-3.604l0,0c-2.246,0-4.066,1.529-4.066,3.604v22.436"/>
 							<path id="right_ring_finger" fill="#FFFFFF" stroke="#3D3D3D" stroke-width="3" stroke-miterlimit="1" d="M151.712,138.071
-							v-18.833c0-2.073-1.82-3.604-4.067-3.604l0,0c-2.246,0-4.067,1.529-4.067,3.604v18.833"/>  
+							v-18.833c0-2.073-1.82-3.604-4.067-3.604l0,0c-2.246,0-4.067,1.529-4.067,3.604v18.833"/>
 						</g>
 					</g>
 				</svg>
